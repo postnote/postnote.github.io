6f3eefe9fc9b96e5a042ffa7077f09e4d231d941
postnote <45489400+postnote@users.noreply.github.com>
2018-11-30 14:43:40 +0100

    Update tabletop.js

--- tabletop.js
+++ tabletop.js
@@ -1,5 +1,5 @@
 function init() {
- Tabletop.init( { key: ‘https://drive.google.com/open?id=1UJcmT-BlIINS_I3nK1mJLmrnjdBLY1I_dpbxxyD1ZAg',
+ Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1UJcmT-BlIINS_I3nK1mJLmrnjdBLY1I_dpbxxyD1ZAg/edit?usp=sharing',
  callback: function(data, tabletop) { 
  console.log(data)
  },
