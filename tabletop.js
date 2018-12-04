3a078469faa8a2f755f2cc5866dcdd249ed6f18e
postnote <45489400+postnote@users.noreply.github.com>
2018-12-04 13:48:03 +0100

    Delete tabletop.js

--- tabletop.js
+++
@@ -1,8 +0,0 @@
-function init() {
- Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1UJcmT-BlIINS_I3nK1mJLmrnjdBLY1I_dpbxxyD1ZAg/edit?usp=sharing',
- callback: function(data, tabletop) { 
- console.log(data)
- },
- simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
